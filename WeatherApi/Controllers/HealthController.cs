54de1750b159c87116ae5ab157373721f41ff395
Лъчезар Атанасов <lachezaratanasov05@pmgkk.com>
2025-10-03 18:06:22 +0300

    Add an endpoint GET /health that returns 200OK.

---
+++ WeatherApi/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApi.Controllers;
+
+/// <summary>
+/// Provides endpoints for health check.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class HealthController : Controller
+{
+    /// <summary>
+    /// Returns OK if the API is available.
+    /// </summary>
+    /// <returns>HTTP 200 response with "OK".</returns>
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("OK");
+    }
+}
