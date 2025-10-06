bb0fa66ca7904964de89c18b74a9efae082c2d1b
Лъчезар Атанасов <lachezaratanasov05@pmgkk.com>
2025-10-06 17:26:05 +0300

    Add a static conversion class to convert between weather dtos service types. Add error handling for `GetWeather` method.

---
+++ WeatherApi/Services/WeatherConversions.cs
@@ -0,0 +1,32 @@
+﻿using WeatherApi.Models;
+using WeatherApi.Models.Dtos;
+
+namespace WeatherApi.Services;
+
+/// <summary>
+/// Contains methods for converting weather models between Dtos and service types.
+/// </summary>
+internal static class WeatherConversions
+{
+    /// <summary>
+    /// Converts an open weather Dto to service type.
+    /// </summary>
+    /// <param name="source">the source</param>
+    /// <returns>the converted weather</returns>
+    public static Weather ConvertWeather(OpenWeatherDto source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        var target = new Weather
+        {
+            Temperature = source.current?.temp ?? 0,
+            Description = source.current?.weather?[0].description ?? "",
+            Icon = source.current?.weather?[0].icon ?? "",
+            Humidity = source.current?.humidity ?? 0,
+            WindSpeed = source.current?.wind_speed ?? 0,
+            Timezone = source.timezone ?? "",
+        };
+
+        return target;
+    }
+}
--- WeatherApi/Services/WeatherService.cs
+++ WeatherApi/Services/WeatherService.cs
@@ -1,4 +1,5 @@
-﻿using WeatherApi.Models;
+﻿using Microsoft.Extensions.Logging;
+using WeatherApi.Models;
 using WeatherApi.Models.Dtos;
 
 namespace WeatherApi.Services;
@@ -8,45 +9,51 @@ namespace WeatherApi.Services;
 /// </summary>
 /// <param name="httpClient">the HTTP client</param>
 /// <param name="config">the configuration</param>
-public class WeatherService(HttpClient httpClient, IConfiguration config)
+/// <param name="logger">the logger</param>
+public class WeatherService(HttpClient httpClient, IConfiguration config, ILogger<WeatherService> logger)
     : IWeatherService
 {
     private readonly HttpClient _httpClient = httpClient;
+    private readonly ILogger<WeatherService> _logger = logger;
     private readonly string _apiKey
         = config["OpenWeather:ApiKey"] ?? throw new ArgumentNullException("API key not configured.");
 
     /// <inheritdoc/>
     public async Task<Weather> GetWeather(double lat, double lon, string[] excludeArray)
     {
-        ArgumentNullException.ThrowIfNull(lat);
-        ArgumentNullException.ThrowIfNull(lon);
-
-        string excludeParts = string.Join(',', excludeArray);
-
-        var url = $"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}" +
-                $"&exclude={excludeParts}&appid={_apiKey}&units=metric";
-
-        var response = await _httpClient.GetAsync(url);
-
-        if (!response.IsSuccessStatusCode)
+        try
         {
-            throw new Exception($"OpenWeather error: {response.StatusCode}");
+            ArgumentNullException.ThrowIfNull(lat);
+            ArgumentNullException.ThrowIfNull(lon);
+
+            string excludeParts = string.Join(',', excludeArray);
+
+            var url = $"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}" +
+                    $"&exclude={excludeParts}&appid={_apiKey}&units=metric";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"OpenWeather error: {response.StatusCode}");
+            }
+
+            OpenWeatherDto? openWeatherDto = await response.Content.ReadFromJsonAsync<OpenWeatherDto>();
+
+            if (openWeatherDto is null)
+            {
+                throw new InvalidOperationException("Could not parse weather data from OpenWeather response.");
+            }
+
+            Weather result = WeatherConversions.ConvertWeather(openWeatherDto);
+
+            return result;
         }
-
-        OpenWeatherDto? openWeatherDto = await response.Content.ReadFromJsonAsync<OpenWeatherDto>();
-
-        ArgumentNullException.ThrowIfNull(openWeatherDto);
-
-        var weather = new Weather
+        catch (Exception ex)
         {
-            Temperature = openWeatherDto.current?.temp ?? 0,
-            Description = openWeatherDto.current?.weather?[0].description ?? "",
-            Icon = openWeatherDto.current?.weather?[0].icon ?? "",
-            Humidity = openWeatherDto.current?.humidity ?? 0,
-            WindSpeed = openWeatherDto.current?.wind_speed ?? 0,
-            Timezone = openWeatherDto.timezone ?? "",
-        };
+            _logger.LogError(ex, "Unexpected error while retrieving weather data.");
 
-        return weather;
+            throw;
+        }
     }
 }
