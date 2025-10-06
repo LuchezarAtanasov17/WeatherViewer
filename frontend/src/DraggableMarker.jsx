fe96a4e61f2c0669ef8376d8748758599d06bdba
Лъчезар Атанасов <lachezaratanasov05@pmgkk.com>
2025-10-06 05:14:01 +0300

    Extract Leaflet marker icon to utils.

--- frontend/src/DraggableMarker.jsx
+++ frontend/src/DraggableMarker.jsx
@@ -1,14 +1,5 @@
 import { Marker } from "react-leaflet";
-import L from "leaflet";
-
-const markerIcon = new L.Icon({
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  shadowSize: [41, 41],
-});
+import markerIcon from "../utils/markerIcon";
 
 function DraggableMarker({ position, onMove }) {
   const handleDragEnd = (e) => {
@@ -20,7 +11,7 @@ function DraggableMarker({ position, onMove }) {
     <Marker
       key={`${position[0]}-${position[1]}`}
       position={position}
-      draggable={true}
+      draggable
       icon={markerIcon}
       eventHandlers={{ dragend: handleDragEnd }}
     />
